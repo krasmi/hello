be82d3b8fd5e661cb4a581a5434e4bf2179e158c
Krasmi <krasmi@gmail.com>
2018-09-24 07:06:28 +0100

    Initial commit

---
+++ src/test/java/csmiles/hello/HelloTest.java
@@ -0,0 +1,14 @@
+package csmiles.hello;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloTest {
+
+    @Test
+    public void testTrue() {
+        assertTrue(true);
+    }
+
+}
